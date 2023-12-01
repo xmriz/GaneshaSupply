e9a0cca3b7f9081876e5a7e8893c0d56d0f0ed9c
Ahmad Rizki <amrizjr@gmail.com>
2023-12-01 22:19:58 +0700

    fix again

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,9 +2,7 @@ import prisma from "../prismaSingleton/prismaSingleClient";
 
 // clear data
 const clearData = async () => {
-  await prisma.allProducts.deleteMany({});
   await prisma.product.deleteMany({});
-  await prisma.order.deleteMany({});
 };
 
 const createProduct = async (
@@ -12,67 +10,33 @@ const createProduct = async (
   price: number,
   description: string
 ) => {
-  const product = await prisma.product.create({
+  await prisma.product.create({
     data: {
       name,
       price,
       description,
     },
   });
-
-  // Create associated AllProducts
-  await prisma.allProducts.create({
-    data: {
-      productId: product.id,
-    },
-  });
-
-  return product;
-};
-
-const createOrder = async (
-  total: number,
-  orderlines: { productId: number; quantity: number }[]
-) => {
-  const order = await prisma.order.create({
-    data: {
-      timestamp: new Date(),
-      total,
-      orderlines: {
-        create: orderlines.map((line) => ({
-          quantity: line.quantity,
-          productId: line.productId,
-        })),
-      },
-    },
-    include: {
-      orderlines: true,
-    },
-  });
-
-  return order;
 };
 
 const main = async () => {
-  // Clear existing data
   await clearData();
-
-  // Create sample products
-  const product1 = await createProduct("Product 1", 20, "Description 1");
-  const product2 = await createProduct("Product 2", 30, "Description 2");
-
-  // Create an order with associated orderlines
-  const order = await createOrder(
-    // total formula
-    product1.price * 2 + product2.price * 1,
-    [
-      { productId: product1.id, quantity: 2 },
-      { productId: product2.id, quantity: 1 },
-    ]
-  );
-
-  console.log("Seeding data created:", { product1, product2, order });
+  await createProduct("Pen", 3000, "This is a pen, you can write with it.");
+  await createProduct("Pencil", 1000, "This is a pencil, you can draw with it.");
+  await createProduct("Eraser", 500, "This is an eraser, you can erase with it.");
+  await createProduct("Notebook", 2000, "This is a notebook, you can write in it.");
+  await createProduct("Book", 5000, "This is a book, you can read it.");  
 };
 
-clearData();
-main();
+main()
+.then(() => {
+    console.log("Seed data complete.");
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
