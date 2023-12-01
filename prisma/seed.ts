16120e081609714f741d5421bfe09a82d9e98775
Ahmad Rizki <amrizjr@gmail.com>
2023-12-01 07:46:41 +0700

    fix prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,78 @@
-import { PrismaClient } from '@prisma/client'
+import prisma from "../prismaSingleton/prismaSingleClient";
 
-const prisma = new PrismaClient()
+// clear data
+const clearData = async () => {
+  await prisma.allProducts.deleteMany({});
+  await prisma.product.deleteMany({});
+  await prisma.order.deleteMany({});
+};
 
-const createProduct = async (name: string, price: number, description: string) => {
-  return await prisma.product.create({
+const createProduct = async (
+  name: string,
+  price: number,
+  description: string
+) => {
+  const product = await prisma.product.create({
     data: {
       name,
       price,
       description,
     },
   });
+
+  // Create associated AllProducts
+  await prisma.allProducts.create({
+    data: {
+      productId: product.id,
+    },
+  });
+
+  return product;
 };
 
+const createOrder = async (
+  total: number,
+  orderlines: { productId: number; quantity: number }[]
+) => {
+  const order = await prisma.order.create({
+    data: {
+      timestamp: new Date(),
+      total,
+      orderlines: {
+        create: orderlines.map((line) => ({
+          quantity: line.quantity,
+          productId: line.productId,
+        })),
+      },
+    },
+    include: {
+      orderlines: true,
+    },
+  });
+
+  return order;
+};
 
 const main = async () => {
-  await createProduct("Pen", 3000, "This is a pen");
-  await createProduct("Pencil", 2000, "This is a pencil");
-  await createProduct("Eraser", 1000, "This is an eraser");
-  await createProduct("Ruler", 5000, "This is a ruler");
-  await createProduct("Notebook", 3500, "This is a notebook");
-  await createProduct("Calculator", 15000, "This is a calculator");
-}
+  // Clear existing data
+  await clearData();
 
-main()
+  // Create sample products
+  const product1 = await createProduct("Product 1", 20, "Description 1");
+  const product2 = await createProduct("Product 2", 30, "Description 2");
+
+  // Create an order with associated orderlines
+  const order = await createOrder(
+    // total formula
+    product1.price * 2 + product2.price * 1,
+    [
+      { productId: product1.id, quantity: 2 },
+      { productId: product2.id, quantity: 1 },
+    ]
+  );
+
+  console.log("Seeding data created:", { product1, product2, order });
+};
+
+clearData();
+main();
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,4 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prismaSingleton/prismaSingleClient";
+
+
