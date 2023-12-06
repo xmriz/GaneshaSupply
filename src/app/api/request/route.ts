dfe07e6ebf2aa98c0509af39dc9281235f79b5c4
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-12-06 14:37:57 +0700

    Prisma bad

---
+++ src/app/api/request/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prismaSingleton/prismaSingleClient";
+
+interface Params {
+  productId: Number;
+}
+
+export async function GET(req: NextRequest, { params }: { params: Params }) {
+  const { productId } = params;
+
+  if (!productId) {
+    return NextResponse.json(
+      { error: "Request ID not provided" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const request = await prisma.request.findUnique({
+      where: { id: productId },
+    });
+
+    if (!request) {
+      return NextResponse.json({ error: "Request not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Request successfully retrieved", request },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error retrieving Request:", error);
+    return NextResponse.json(
+      { error: "Failed to retrieve Request" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  //   create request
+  const body = await req.json();
+
+  //   validate body?
+  if (!body.productId || !body.timeReq || !body.amount) {
+    return NextResponse.json({ error: "Invalid body" }, { status: 400 });
+  }
+
+  try {
+    const request = await prisma.request.create({
+      data: {
+        productId : body.productId,
+        timeReq: body.timeReq,
+        amount : body.amount,
+      },
+    });
+    return NextResponse.json(
+      { message: "request successfully created", request },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error creating request:", error);
+    return NextResponse.json(
+      { error: "Failed to create request" },
+      { status: 500 }
+    );
+  }
+}
